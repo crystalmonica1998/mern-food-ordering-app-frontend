7fd6a517f5d403079ab25fa93bedfcf13eb31c04
ABOU JNEID Crystal-Monica <brt.cmaboujneid@cma-cgm.com>
2024-12-08 14:25:18 +0200

    add filters

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,45 +1,56 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import Layout from "./layouts/layout";
-import HomePage from "./pages/HomePage";
-import AuthCallbackPage from "./pages/AuthCallbackPage";
-import UserProfilePage from "./pages/UserProfilePage";
-import ProtectedRoute from "./auth/ProtectedRoute";
-import ManageRestaurantPage from "./pages/ManageRestaurantPage";
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Layout from './layouts/layout'
+import HomePage from './pages/HomePage'
+import AuthCallbackPage from './pages/AuthCallbackPage'
+import UserProfilePage from './pages/UserProfilePage'
+import ProtectedRoute from './auth/ProtectedRoute'
+import ManageRestaurantPage from './pages/ManageRestaurantPage'
+import SearchPage from './pages/SearchPage'
 
 const AppRoutes = () => {
-    return (
-        <Routes>
-            <Route 
-                path='/' 
-                element={
-                    <Layout showHero>
-                        <HomePage />
-                    </Layout>
-                }
-            />
-            <Route path='/auth-callback' element={<AuthCallbackPage />} />
-            <Route element={<ProtectedRoute />}>
-                <Route 
-                    path='/user-profile' 
-                    element={
-                        <Layout showHero={false}>
-                            <UserProfilePage />
-                        </Layout>
-                    } 
-                />
+  return (
+    <Routes>
+      <Route
+        path='/'
+        element={
+          <Layout showHero>
+            <HomePage />
+          </Layout>
+        }
+      />
+      <Route path='/auth-callback' element={<AuthCallbackPage />} />
 
-                <Route 
-                    path='/manage-restaurant' 
-                    element={
-                        <Layout showHero={false}>
-                            <ManageRestaurantPage />
-                        </Layout>
-                    } 
-                />
-            </Route>
-            <Route path='*' element={<Navigate to='/' />} />
-        </Routes>
-    );
-};
+      <Route
+        path='/search/:city'
+        element={
+          <Layout showHero={false}>
+            <SearchPage />
+          </Layout>
+        }
+      />
 
-export default AppRoutes;
+      <Route element={<ProtectedRoute />}>
+        <Route
+          path='/user-profile'
+          element={
+            <Layout showHero={false}>
+              <UserProfilePage />
+            </Layout>
+          }
+        />
+
+        <Route
+          path='/manage-restaurant'
+          element={
+            <Layout showHero={false}>
+              <ManageRestaurantPage />
+            </Layout>
+          }
+        />
+      </Route>
+      <Route path='*' element={<Navigate to='/' />} />
+    </Routes>
+  )
+}
+
+export default AppRoutes
