3891126c1a6084ced78a5ed71061f026779aff45
ABOU JNEID Crystal-Monica <brt.cmaboujneid@cma-cgm.com>
2025-01-10 21:41:09 +0200

    add view order api integration

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,4 +1,6 @@
-import { useCreateMyRestaurant, useGetMyRestaurant, useUpdateMyRestaurant } from "@/api/MyRestaurantApi"
+import { useCreateMyRestaurant, useGetMyRestaurant, useGetMyRestaurantOrders, useUpdateMyRestaurant } from "@/api/MyRestaurantApi"
+import OrderItemCard from "@/components/OrderItemCard";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm"
 
 const ManageRestaurantPage = () => {
@@ -6,17 +8,33 @@ const ManageRestaurantPage = () => {
   const { restaurant } = useGetMyRestaurant();
   const { updateRestaurant, isLoading: isUpdateLoading } = useUpdateMyRestaurant();
 
-  // Checking if the restaurant already exists for that user
-  // if there is a restaurant this expression is going to return true
-  // if restaurant is undefined this expression is going to return false
+  const { orders } = useGetMyRestaurantOrders();
+
   const isEditing = !!restaurant;
 
   return (
-    <ManageRestaurantForm
-      onSave={isEditing ? updateRestaurant : createRestaurant}
-      isLoading={isCreateLoading || isUpdateLoading}
-      restaurant={restaurant}
-    />
+    <Tabs defaultValue="orders">
+      <TabsList>
+        <TabsTrigger value='orders'>Orders</TabsTrigger>
+        <TabsTrigger value='manage-restaurant'>Manage Restaurant</TabsTrigger>
+      </TabsList>
+
+      <TabsContent value='orders' className="space-y-5 bg-gray-50 pg-10 rounded-lg">
+        <h2 className="text-2xl font-bold">{orders?.length} active orders</h2>
+
+        {orders?.map((order) => (
+          <OrderItemCard order={order} />
+        ))}
+      </TabsContent>
+
+      <TabsContent value='manage-restaurant'>
+        <ManageRestaurantForm
+          onSave={isEditing ? updateRestaurant : createRestaurant}
+          isLoading={isCreateLoading || isUpdateLoading}
+          restaurant={restaurant}
+        />
+      </TabsContent>
+    </Tabs>
   )
 }
 
